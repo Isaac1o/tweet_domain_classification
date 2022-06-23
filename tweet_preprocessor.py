d1d2f6c065c5c8866b79199e9df03794c10248db
Isaac Lo <isaac.lo9911@gmail.com>
2022-06-23 16:05:00 -0700

    completed function to load from s3

--- tweet_preprocessor.py
+++ tweet_preprocessor.py
@@ -7,7 +7,7 @@ import boto3
 
 
 def get_files(client, bucket_name, prefix):
-    # Returns all files with data
+    # Returns all filenames within bucket
     paginator = client.get_paginator('list_objects_v2')
     response = paginator.paginate(Bucket=bucket_name, Prefix=prefix, PaginationConfig={"PageSize": 1000})
     all_files = []
@@ -16,22 +16,26 @@ def get_files(client, bucket_name, prefix):
         for file in files:
             all_files.append(file['Key'])
 
-    return all_files
+    return all_files[1:]
 
 
-def parse_data(filepath):
-    rows = []
-    with open(filepath, 'r') as f:
-        for s in f.readlines():
-            row = s.split('<COMMA>')
-            row = process_row(row)
-            rows.append(row)
+def parse_data(bucket, prefix):
+    # Loads text file from bucket and parses the file and decodes the tweet
+    data = s3_client.get_object(Bucket=bucket, Key=prefix)  # Read data from s3
+    contents = data['Body'].read()  # Get the data from the data object
+    rows = contents.split('\n')
 
-    return rows
+    processed_rows = []
+    for row in rows:
+        row = row.split('<COMMA>')
+        row = process_row(row)
+        processed_rows.append(row)
+
+    return processed_rows  # List of tweets and their categories
 
 
 def process_row(row):
-    # The first element of the row is a byte string
+    # The first element of the row is a byte string. This
     row[0] = ast.literal_eval(row[0]).decode('utf-8')
     return [elem.strip('\n') for elem in row]
 
@@ -48,7 +52,9 @@ if __name__ == '__main__':
         aws_access_key_id=os.environ['AWS_ACCESS_KEY_NLP'],
         aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY_NLP']
     )
+
     BUCKET = 'deeplearningtweetdata'
     PREFIX = 'data/multi_label/'
     files = get_files(s3_client, BUCKET, PREFIX)
+    print(parse_data(BUCKET, files[0]))
     print(files)
